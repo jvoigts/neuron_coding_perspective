ddeef25303afd3b3d134637fdaa5416c40a4ac7d
Jakob Voigts <jvoigts@mit.edu>
2015-12-09 19:53:20 -0500

    added correlations
    added mostly pairwise correlations while keeping sigma pos.semidefinite

--- example_figure.m
+++ example_figure.m
@@ -6,7 +6,8 @@ ratemax=10;
 ntrials=100;
 
 nstims=5;
-ncells=4;
+ncells=6;
+plotcells=[4 5]; 
 
 ll=linspace(0,ratemax,50);
 %
@@ -14,12 +15,13 @@ fstrs={'b'};
 % simulate two cells with and without correlation
 
 for stim=1:nstims
-    mu{stim}=ones(ncells,1)*stim;
+    mu{stim}=1+ones(1,ncells)*stim;
 end;
 
-cv= -0.3;
+cv= .9;
 
-sigma =(ones(ncells)*cv) .* (1-eye(ncells)) +eye(ncells);
+sigma=diag(ones(ncells-1,1)*cv,-1)+diag(ones(ncells-1,1)*cv,1)+eye(ncells);
+sigma_p=(sigma*sigma')/2; % make positive semidefinite
 
 x=linspace(0,2*pi,100)';
 circle=[sin(x),cos(x)]*2;
@@ -27,19 +29,20 @@ circle=[sin(x),cos(x)]*2;
 figure (1); clf;
 subplot(2,2,1);
 hold on;
-for stim=1:nstims
+
+for stim=[2 5]
     
     thiscolor=0.5+([sin(stim),-sin(stim*.6),-sin(stim*1.2)]/2);
     
-    rates{stim} = mvnrnd(mu{stim},sigma,ntrials);
+    rates{stim} = mvnrnd(mu{stim},sigma_p,ntrials);
     
-    plot(rates{stim}(:,1),rates{stim}(:,2),[fstrs{1} '.'],'color',thiscolor);
+    plot(rates{stim}(:,plotcells(1)),rates{stim}(:,plotcells(2)),[fstrs{1} '.'],'color',thiscolor);
     
-    plot(ll,normpdf(ll,mu{stim}(1),sigma(1,1)),fstrs{1},'color',thiscolor);
-    plot(normpdf(ll,mu{stim}(2),sigma(2,2)),ll,fstrs{1},'color',thiscolor);
+    plot(ll,normpdf(ll,mu{stim}(plotcells(1)),sigma(plotcells(1),plotcells(1))),fstrs{1},'color',thiscolor);
+    plot(normpdf(ll,mu{stim}(plotcells(2)),sigma(plotcells(2),plotcells(2))),ll,fstrs{1},'color',thiscolor);
     
-    cc=circle*sigma(1:2,1:2);
-    plot( cc(:,1)+mu{stim}(1),cc(:,2)+mu{stim}(2),[fstrs{1} '-'],'color',thiscolor);
+    cc=circle*sigma_p(plotcells,plotcells);
+    plot( cc(:,1)+mu{stim}(plotcells(1)),cc(:,2)+mu{stim}(plotcells(2)),[fstrs{1} '-'],'color',thiscolor);
     
     %h=histc(r(:,1),ll);
     %plot(ll,h,'b');
@@ -56,11 +59,16 @@ hold on;
 trialwidth=.3;
 for stim=1:nstims
     
+    rates{stim} = mvnrnd(mu{stim},sigma_p,ntrials);
+    
     thiscolor=0.5+([sin(stim),-sin(stim*.6),-sin(stim*1.2)]/2);
     
     plot(stim+[-trialwidth trialwidth],[1 1]*mu{stim}(1),'color',thiscolor);
     
     ll=linspace(-trialwidth,trialwidth,ncells);
-    plot(ll+stim,rates{stim}(1,:),'k.');
+    
+    plot(ll+stim,rates{stim}(1,:),'k.','MarkerSize',10);
+    
+    plot(stim+[-trialwidth trialwidth],[1 1]*mean(rates{stim}(1,:)) ,'k--');
     
 end;
