4d04f63600f9912a0be4f89dc0612f4edbde130b
Jakob Voigts <jvoigts@mit.edu>
2015-12-09 22:51:45 -0500

    added cv vs per trial error plot

--- example_figure.m
+++ example_figure.m
@@ -1,10 +1,11 @@
-%/2
+% make simple exaexamples of a few more or less correlated cells
+% and see how it affects decoding
 %
 %
-rng(11) % For reproducibility
 ratemax=10;
 ntrials=100;
 
+rng(5) % For reproducibility
 nstims=5;
 tuning=normpdf([1:nstims],3.2,1);
 tuning=10*(tuning./max(tuning));
@@ -12,7 +13,7 @@ tuning=10*(tuning./max(tuning));
 ncells=6;
 plotcells=[3 4];
 
-cvs=[0 0.4];
+cvs=[.4 0  -0.4];
 
 ll=linspace(0,ratemax,50);
 %
@@ -40,19 +41,19 @@ for cv_i=1:numel(cvs)
     x=linspace(0,2*pi,100)';
     circle=[sin(x),cos(x)]*2;
     
-    subplot(2,3,1+((cv_i-1)*3));
+    subplot(numel(cvs),3,1+((cv_i-1)*3));
     hold on;
     
     for stim=[2 5]
         
-        thiscolor=0.5+([sin(stim),-sin(stim*.6),-sin(stim*1.2)]/2);
+        thiscolor=0.5+([sin(stim),-sin(stim*.2),-sin(stim*1.2)]/2);
         
         rates{stim} = mvnrnd(mu{stim},sigma_p,ntrials);
         
         plot(rates{stim}(:,plotcells(1)),rates{stim}(:,plotcells(2)),[fstrs{1} '.'],'color',thiscolor);
         
-        plot(ll,normpdf(ll,mu{stim}(plotcells(1)),sigma(plotcells(1),plotcells(1))),fstrs{1},'color',thiscolor);
-        plot(normpdf(ll,mu{stim}(plotcells(2)),sigma(plotcells(2),plotcells(2))),ll,fstrs{1},'color',thiscolor);
+        plot(ll,2*normpdf(ll,mu{stim}(plotcells(1)),sigma(plotcells(1),plotcells(1))),fstrs{1},'color',thiscolor);
+        plot(2*normpdf(ll,mu{stim}(plotcells(2)),sigma(plotcells(2),plotcells(2))),ll,fstrs{1},'color',thiscolor);
         
         cc=circle*sigma_p(plotcells,plotcells);
         plot( cc(:,1)+mu{stim}(plotcells(1)),cc(:,2)+mu{stim}(plotcells(2)),[fstrs{1} '-'],'color',thiscolor);
@@ -67,7 +68,7 @@ for cv_i=1:numel(cvs)
     xlabel(['cell ',num2str(plotcells(1)),' rate']);
     ylabel(['cell ',num2str(plotcells(2)),' rate']);
     
-    subplot(2,3,2+((cv_i-1)*3));
+    subplot(numel(cvs),3,2+((cv_i-1)*3));
     hold on;
     %
     
@@ -90,4 +91,36 @@ for cv_i=1:numel(cvs)
     ylim([0 15]);
     ylabel('Firing rates across cells');
     xlabel('Trials/stimuli');
+    
+    %subplot(numel(cvs),3,3+((cv_i-1)*3));
+    %hold on;
+    
+    
 end;
+
+
+cvs_dense=linspace(-.6,.6,100);
+meanerr=[];
+ntrials_dense=10000;
+for cv_i=1:numel(cvs_dense)
+    
+    cv= cvs_dense(cv_i);
+    
+    sigma=diag(ones(ncells-1,1)*cv,-1)+diag(ones(ncells-1,1)*cv,1)+eye(ncells);
+    sigma_p=(sigma*sigma'); % make positive semidefinite
+    for stim=1:nstims
+        rates{stim} = mvnrnd(mu{stim},sigma_p,ntrials_dense);
+        
+        sigma_p=sigma_p./mean(diag(sigma_p));
+        meanerr(cv_i,stim)= mean(abs(mean((rates{stim} - repmat(mu{stim},ntrials_dense,1))')));
+    end;
+end;
+
+subplot(numel(cvs),3,3);
+plot(cvs_dense,mean(meanerr'));
+ylim([0 .6]);
+grid on;
+ylabel('mean per-trial error from mean');
+    xlabel('correlation');
+    
+    
